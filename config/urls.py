4fe910a44bb3654feb54eef2c49e97897f9fcdef
Annabse27 <annabse27@gmail.com>
2024-12-10 18:10:58 +0100

    КОНФИГУРАЦИОННЫЙ ПАКЕТ:
    настраиваем с приложением пользователей, прохождением тестов

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Online Boutique API",
@@ -18,10 +17,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),  # Ваше приложение users
+    path('api/users/', include('users.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # Дополнительно для Redoc
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/schema/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+
